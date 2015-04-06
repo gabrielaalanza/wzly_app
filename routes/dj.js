87c9f93f0c896833b5fca0de140784a45844fa05
Gabriela Lanza <glanza@wellesley.edu>
2015-04-05 23:27:34 -0400

    Get file extension

--- routes/dj.js
+++ routes/dj.js
@@ -157,6 +157,7 @@ module.exports = function(passport){
                 console.log(err);
             }
             else{
+                console.log("extension name: "+path.extname(req.query.s3_object_name).toLowerCase());
                 var return_data = {
                     signed_request: data,
                     url: 'https://'+S3_BUCKET+'.s3.amazonaws.com/'+req.query.s3_object_name
