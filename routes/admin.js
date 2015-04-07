13371d403edc2738f26fae5e0d363efadc461e8a
Gabriela Lanza <glanza@wellesley.edu>
2015-04-06 23:22:56 -0400

    Get last user call out

--- routes/admin.js
+++ routes/admin.js
@@ -557,7 +557,6 @@ module.exports = function(passport){
                 console.log(err);
             }
             else{
-                console.log(req.user.id+path.extname(req.query.s3_object_name).toLowerCase());
                 var return_data = {
                     signed_request: data,
                     url: 'https://'+S3_BUCKET+'.s3.amazonaws.com/spam/'+req.query.event_id+path.extname(req.query.s3_object_name).toLowerCase()
