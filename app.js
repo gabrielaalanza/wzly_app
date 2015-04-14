c719ffa4b7ae5f460408520db1b351525cb0a00e
Gabriela Lanza <glanza@wellesley.edu>
2015-04-13 20:08:35 -0400

    take out logging

--- app.js
+++ app.js
@@ -89,7 +89,7 @@ app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
+//app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(multer({
