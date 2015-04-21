13abf025320aba64a1eaddc4a34fe2295b476e6a
Gabriela Lanza <glanza@wellesley.edu>
2015-04-20 22:08:38 -0400

    Tweaks to megaseg

--- routes/automation.js
+++ routes/automation.js
@@ -6,66 +6,66 @@ var Song = require('../models/song');
 
 module.exports = function(){
 
-  router.post('/log-automated/:data', function(req, res){
+  router.route('/log-automated/:data')
+    .post(function(req, res){
 
-    console.log(req);
-    
-    //save the song
-    var song = new Song();
-    song.name = req.body.name;
-    song.artist = req.body.artist;
-    song.album = req.body.album;
+        console.log(req.params.data);
+        
+        //save the song
+        var song = new Song();
+        song.name = req.body.name;
+        song.artist = req.body.artist;
+        song.album = req.body.album;
 
-    console.log("Saving song: "+song+'\n');
+        console.log("Saving song: "+song+'\n');
 
-    var position;
+        var position;
 
-    Song.nextCount(function(err, count) {
+        Song.nextCount(function(err, count) {
 
-        song.save(function(err, result){
-            if(err) {
-                console.log("There was an error adding this song: "+err);
-            } else {
+            song.save(function(err, result){
+                if(err) {
+                    console.log("There was an error adding this song: "+err);
+                } else {
 
-                console.log('Song Added');
-            }
-        });
-        console.log("Song position is: "+count+'\n');
-        position = count;
-    });
-
-    //then, update user profile to claim this song in playlist
-    var query = {'_id': req.user.id};
-
-    User.findOne(query, function(err, user) {
-
-        //get the playlist
-
-        var playlist = user.playlists[user.playlists.length-1];
-        console.log("Playlist: "+playlist+'\n');
-
-        //check to see if this is the first song in the playlists
-        if(typeof playlist.startIndex == 'undefined') {
-            //if so, update the start index
-            playlist.startIndex = position;
-
-        } else {
-            //if not, update the end index
-            playlist.endIndex = position;
-        }
-
-        user.save(function(err, result){
-            if(err) {
-                console.log('error saving ('+user.local.username+') playlist index: '+err);
-            } else {
-
-                console.log(user.local.username+"'s playlist has been saved \n");
-            }
+                    console.log('Song Added');
+                }
+            });
+            console.log("Song position is: "+count+'\n');
+            position = count;
         });
 
-    });
+        //then, update user profile to claim this song in playlist
+        var query = {'_id': req.user.id};
 
-    res.redirect('back');
+        User.findOne(query, function(err, user) {
+
+            //get the playlist
+
+            var playlist = user.playlists[user.playlists.length-1];
+            console.log("Playlist: "+playlist+'\n');
+
+            //check to see if this is the first song in the playlists
+            if(typeof playlist.startIndex == 'undefined') {
+                //if so, update the start index
+                playlist.startIndex = position;
+
+            } else {
+                //if not, update the end index
+                playlist.endIndex = position;
+            }
+
+            user.save(function(err, result){
+                if(err) {
+                    console.log('error saving ('+user.local.username+') playlist index: '+err);
+                } else {
+
+                    console.log(user.local.username+"'s playlist has been saved \n");
+                }
+            });
+
+        });
+
   })
 
 return router;
