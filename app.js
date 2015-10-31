dc5ed809415850fa4fe22e018fabfec927ac96cb
Gabriela Lanza <glanza@wellesley.edu>
2015-10-31 11:19:26 -0400

    added cors back in

--- app.js
+++ app.js
@@ -77,14 +77,14 @@ initPassport(passport);
 /***********************/
 
 // CORS
-/*
+
 app.all('*', function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
-*/
+
 // require routes
 var routes = require('./routes/routes')(passport);
 var admin = require('./routes/admin')(passport);
