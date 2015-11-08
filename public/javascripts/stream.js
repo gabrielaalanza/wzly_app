8cb1616bc9ee79d430a80d66401a7409cdd9325d
Gabriela Lanza <glanza@wellesley.edu>
2015-11-08 14:27:01 -0500

    take out crazy console.log

--- public/javascripts/stream.js
+++ public/javascripts/stream.js
@@ -58,7 +58,6 @@ function checkForUpdates() {
 					if (album == "" || album == undefined) album = "";
 
 		            trHTML += '<tr><td><span data-toggle="tooltip" data-placement="top" title="Played by: '+item.playedBy+'">' + name + '</span></td><td>' + artist + '</td><td>' + album + '</td><td>' + display + '</td></tr>';
-		        	console.log(trHTML);
 		        });
 
 		        $('.table-albums').append(trHTML);
